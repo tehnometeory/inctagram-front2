de401de5d1f6a1424265e4813f85e200163138f3
Dmitriy <goglev37@gmail.com>
2024-12-22 11:59:50 +0300

    Proj-66 feat : add my-profile page

--- shared/hooks/useMenuHandlers.ts
+++ shared/hooks/useMenuHandlers.ts
@@ -1,7 +1,12 @@
+import { RoutesApp } from '@/shared'
+
 import { useNRouter } from './useNRouter'
 
 export const useMenuHandlers = () => {
   const router = useNRouter()
 
-  return [{ itemCallback: () => router.push('/statistics'), name: 'Statistics' }]
+  return [
+    { itemCallback: () => router.push('/statistics'), name: 'Statistics' },
+    { itemCallback: () => router.push(RoutesApp.profile), name: 'My Profile' },
+  ]
 }
