f51f9e21d76a49515c2f0674888f96d6105f72ed
Dmitriy <goglev37@gmail.com>
2025-01-21 15:25:10 +0300

    Proj-66 refactor: refactor profile page

--- features/profileUser/ProfileUser.tsx
+++ features/profileUser/ProfileUser.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Button } from '@rambo-react/ui-meteors'
+import { clsx } from 'clsx'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
 
@@ -12,44 +13,50 @@ export const ProfileUser = () => {
   const { data } = useProfileUserByIdQuery(userId as string)
 
   if (!data) {
-    return
+    return null
   }
 
   return (
-    <div className={s.profile}>
-      <div className={s.container}>
-        <Image
-          alt={'user avatar'}
-          className={s.avatar}
-          height={204}
-          src={'/images/avatar-default.webp'}
-          width={204}
-        />
-        <div className={s.infoProfileWrapper}>
-          <div className={s.infoProfile}>
-            <span className={s.titleProfile}>{data.username}</span>
-            <Button className={s.btn} variant={'secondary'}>
-              Profile Settings
-            </Button>
-          </div>
-          <div className={s.infoStatistic}>
-            <span className={s.statistic}>
-              {data.profileFollowing}
-              <br />
-              <small className={s.textStatistic}>Following</small>
-            </span>
-            <span className={s.statistic}>
-              {data.profileFollowers} <br />
-              <small className={s.textStatistic}> Followers</small>
-            </span>
-            <span className={s.statistic}>
-              {data.postsCount}
-              <br /> <small className={s.textStatistic}>Publications</small>
-            </span>
-          </div>
-          <p className={s.textInfo}>{data.aboutMe}</p>
+    <div className={s.profileUser}>
+      <div className={s.profile}>
+        <div className={clsx(s.item, s.itemImage)}>
+          <Image
+            alt={'user avatar'}
+            className={s.avatar}
+            height={204}
+            src={'/images/avatar-default.webp'}
+            width={204}
+          />
+        </div>
+        <div className={clsx(s.item, s.itemNameProfile)}>
+          <span className={s.titleProfile}>{data.username}</span>
+          <Button className={s.btn} variant={'secondary'}>
+            Profile Settings
+          </Button>
+        </div>
+        <div className={clsx(s.item, s.itemStaticProfile)}>
+          <span className={s.statistic}>
+            {data.profileFollowing}
+            <br />
+            <small className={s.textStatistic}>Following</small>
+          </span>
+          <span className={s.statistic}>
+            {data.profileFollowers} <br />
+            <small className={s.textStatistic}> Followers</small>
+          </span>
+          <span className={s.statistic}>
+            {data.postsCount}
+            <br /> <small className={s.textStatistic}>Publications</small>
+          </span>
+        </div>
+        <div className={clsx(s.item, s.itemAboutMe)}>
+          <p className={s.textInfo}>
+            {data.aboutMe} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt. laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
         </div>
       </div>
+      <div className={s.posts}></div>
     </div>
   )
 }
